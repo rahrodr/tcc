15e0cde39c53732bfff8dd74d14fc9dd2adb8cd7
rahrodr <141769998+rahrodr@users.noreply.github.com>
2023-11-22 15:12:45 -0300

    att

--- menu.php
+++ menu.php
@@ -59,7 +59,7 @@
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                   <li class="nav-item">
-                    <a href="menu.html" class="nav-link align-middle px-0">
+                    <a href="menu.php" class="nav-link align-middle px-0">
                         <i class="bi bi-house-fill"></i>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
                           <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
@@ -68,7 +68,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="conversas.html" class="nav-link align-middle px-0">
+                    <a href="conversas.php" class="nav-link align-middle px-0">
                         <i class="bi bi-gem"></i>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gem" viewBox="0 0 16 16">
                         <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM7.194 6.766a1.688 1.688 0 0 0-.227-.272 1.467 1.467 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 5.734 6C4.776 6 4 6.746 4 7.667c0 .92.776 1.666 1.734 1.666.343 0 .662-.095.931-.26-.137.389-.39.804-.81 1.22a.405.405 0 0 0 .011.59c.173.16.447.155.614-.01 1.334-1.329 1.37-2.758.941-3.706a2.461 2.461 0 0 0-.227-.4zM11 9.073c-.136.389-.39.804-.81 1.22a.405.405 0 0 0 .012.59c.172.16.446.155.613-.01 1.334-1.329 1.37-2.758.942-3.706a2.466 2.466 0 0 0-.228-.4 1.686 1.686 0 0 0-.227-.273 1.466 1.466 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 10.07 6c-.957 0-1.734.746-1.734 1.667 0 .92.777 1.666 1.734 1.666.343 0 .662-.095.931-.26z"/>
@@ -76,7 +76,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="comunidades.html" class="nav-link align-middle px-0">
+                    <a href="comunidades.php" class="nav-link align-middle px-0">
                       <i class="bi bi-chat-quote-fill"></i>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-quote-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
@@ -84,7 +84,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="config.html" class="nav-link align-middle px-0">
+                    <a href="configuracoes.php" class="nav-link align-middle px-0">
                         <i class="bi bi-gear-fill"></i>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
                             <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
@@ -100,17 +100,17 @@
                         </div>
                           <br/>
                 <div class="dropdown pb-2">
-                  <a href="perfilamg.html" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
+                  <a href="perfilamg.php" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
                     <img src="imgs/iconamg.jpg" alt="foto de Ray" width="40" height="45" class="rounded-circle">
                     <span class="contentleft">@rahrodr___</span>
                 </a>
                 <br>
-                <a href="perfilamg.html" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
+                <a href="perfilamg.php" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
                   <img src="imgs/iconamg2.jpg" alt="foto de Ray" width="40" height="45" class="rounded-circle">
                   <span class="contentleft">@carlss33</span>
               </a>
               <br>
-              <a href="perfilamg.html" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
+              <a href="perfilamg.php" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
                 <img src="imgs/iconamg3.jpg" alt="foto de Ray" width="40" height="45" class="rounded-circle">
                 <span class="contentleft">@j3an_n</span>
             </a>
@@ -123,7 +123,7 @@
             <h3 id="titulo" class="fw-bold"> <img src="https://i.postimg.cc/V6TH3xvV/luru.png" width="50" height="50" text-align="left">
               Seja bem-vindo(a), user!<br/>
             </h3>
-            <a href="conversas.html">
+            <a href="conversas.php">
             <button type="button" class="btnmsgs">
                 Mensagens
                 <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
